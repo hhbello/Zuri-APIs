995e71a0c47b10b95df42cb176184e48a30ef41f
Hafeez Husain Bello <105268549+hhbello@users.noreply.github.com>
2022-06-30 16:30:44 +0800

    DRF

---
+++ I4G005469CNJ/links/views.py
@@ -0,0 +1,29 @@
+from links.models import Link
+from links.serializers import LinkSerializer
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
+
+
+# Create your views here.
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
